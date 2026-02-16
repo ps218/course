cdb0fb67e13725e9b8752f8c2b6685a06d2df950
ps218 <stawer2018@gmail.com>
2026-02-16 20:52:20 +0100

    x2

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -19,4 +19,14 @@ where review_text is not null
 -- if is incremental then append below sql part
 and review_date > (select max(review_date)from {{this}}) 
 --selecting only dates bigger then already existing in the model before its remateerialization
+--
+/*  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
+ */ --
 {% endif %}
